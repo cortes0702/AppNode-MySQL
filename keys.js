fd9777f1f84408b78062f04b7b9481b215b5580b
Jaume Cortés <79199660+cortes0702@users.noreply.github.com>
2022-02-23 19:21:39 +0100

    Update keys.js

--- keys.js
+++ keys.js
@@ -1,7 +1,7 @@
 module.exports = {
 
     database: {
-        host: 'localhost',
+        host: 'mysql://uk9vfitom9lizaeu:GK0lXlwrXzCerSpjPhCk@bnus4cctdluq9rwd2s0g-mysql.services.clever-cloud.com:3306/bnus4cctdluq9rwd2s0g',
         user: 'root',
         password: 'root',
         database: 'database_links'
